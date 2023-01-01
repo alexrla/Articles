99f43b106779f9996d19a76455fb98dbac7d8766
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-12-31 22:44:31 -0300

    Criado os modais no arquivo index.html

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// CKEditor
+ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .then( editor => {
+                    console.log( editor );
+            } )
+            .catch( error => {
+                    console.error( error );
+            } );
